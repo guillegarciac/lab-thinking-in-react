75e6a97e83a02b149e1d88fde6309914b08eec15
guille garcia <ggarciacardiel@gmail.com>
2023-03-03 15:15:57 +0100

    it1 creates components and relationships

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import React, { useState } from 'react'
+import SearchBar from './components/SearchBar';
+import ProductTable from './components/ProductTable';
 
 function App() {
   return (
     <div className="App">
-      
+      {/* TO BE USED */}
+      <SearchBar></SearchBar>
+      <ProductTable></ProductTable>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/ProductTable.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from 'react'
+import ProductRow from './ProductRow'
+
+export default function ProductTable({xxx}) {
+}
