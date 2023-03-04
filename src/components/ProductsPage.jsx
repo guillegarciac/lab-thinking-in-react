12e909a3874dd66b274549dd45a813e9e62c41ea
guille garcia <ggarciacardiel@gmail.com>
2023-03-04 08:18:07 +0100

    it5 checkbox filter

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -5,16 +5,28 @@ import ProductTable from './ProductTable';
 
 export default function ProductsPage() {
   const [products, setProducts] = useState(jsonData);
+  const [checked, setChecked] = useState(true); //true to ensure all products are shown to the user by default. Otherwise, handleStockSearch() would be triggered with "checked=false".
   
   const handleSearch = (str) => {
     const filteredProducts = products.filter(product => product.name.toLowerCase().includes(str.target.value.toLocaleLowerCase()))
     setProducts(filteredProducts)
   }
 
+  const handleStockSearch = (checked) => {
+    if (checked) { //if true then we filter products by inStock property
+      const availableProducts = products.filter(product => product.inStock);
+      setProducts(availableProducts) //we update the products to show
+      setChecked(!checked); // update the state of checked to as its opposite otherwise when we uncheck it from the UI => the not.inStock won't come back
+    } else {
+      setProducts(jsonData);
+      setChecked(!checked)
+    }
+  }
+  
   return (
     <div>
       <h1>IronStore</h1>
-      <SearchBar onChange={handleSearch} />
+      <SearchBar onChange={handleSearch} onCheck={handleStockSearch} checked={checked} />
       <ProductTable products={products} />
     </div>    
   )
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 
 export default function SearchBar(props) {
-  const {onChange} = props;
+  const {onChange, onCheck, checked} = props;
 
   return (
     <div>
       <input value={undefined} type="text" placeholder="search product..." onChange={onChange} />
-      <input type="checkbox"/><span>Only show products in stock</span>
+      <input type="checkbox" onChange={()=> onCheck(checked)}/><span>Only show products in stock</span>
     </div>
   )
 }
